10681dac07433d728a2c4ff538d0d2d6556c078c
Tahsin Ahmed <54328406+Tahsin-Ahmed52225@users.noreply.github.com>
2020-01-25 02:54:59 +0600

    Add files via upload

---
+++ login/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+from .models import account
+
+admin.site.register(account)
---
+++ login/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+
+class account(models.Model) :
+    name = models.CharField(max_length = 100)
+    email = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    job = models.CharField(max_length=20)
+    passwd = models.CharField(max_length = 100)
+
+
